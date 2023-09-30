c0a2b26901cf712a10bbc34187a59d8b19afbe7f
AxoltDash <darshan@ciencias.unam.mx>
2023-09-30 17:19:19 -0600

    :tada: GG!

--- Proyecto01_Pérez_Darshan/practica/main.py
+++ Proyecto01_Pérez_Darshan/practica/main.py
@@ -6,35 +6,58 @@ import unicodedata #Para evitar problemas en los strings de operaciónes
 from src.genera_archivos import enteros_aleatorios
 from src.borrador import borrar_archivos
 
+#Creo un string para que se vea mejor la terminal:
+lineaDeIguales = "==============================================================="
+
 print("\n/////////////////////////////////////////////////////////")
 print("-= ¡BIENVENID@ AL PROGRAMA DE OPERACIONES MATEMÁTICAS! =-")
 print("/////////////////////////////////////////////////////////\n")
+print(lineaDeIguales)
 
 def obtener_datos():
     #Obtiene los datos de los 2 argumentos
-    if len(sys.argv) < 3:
+    if len(sys.argv) != 3:
         print("Uso: python programa.py <operacion> <entero>")
         sys.exit(1)
     operacionArg = sys.argv[1] #suma, resta, multi, divi.
-    enteroArg = int(sys.argv[2]) #numEntero
+    enteroArg = sys.argv[2] #numEntero
 
-    # return ("SUma   ", 10) BLOQUE DE CÓDIGO PARA PRUEBAS
-    return operacionArg, enteroArg
+    if not enteroArg.isdigit():
+        print("¡Ocurrió un ERROR! El segundo argumento debe ser un número entero.")
+        sys.exit(1)
+
+    return operacionArg, int(enteroArg)
 
 def main():
+    print(lineaDeIguales)
+
+    #=============================================================================
+    #Mandamos error si no mandó 3 argumentos
+    if len(sys.argv) < 3 or len(sys.argv) > 3:
+        print("¡Ocurrió un ERROR! Debe proporcionar exactamente 2 argumentos.")
+        print(lineaDeIguales)
+
+        print("Uso: python programa.py <texto> <numero>")
+        sys.exit(1)
+    
+    #Obtenemos los datos de los argumentos:
     operacion, entero = obtener_datos()
 
     #=============================================================================
 
     #Guardamos los enteros de datos.txt en una lista llamada lista_numeros
-    with open("datos.txt", "r") as archivoDatos:
-        lista_numeros = []
-        # Lee cada línea
-        for linea in archivoDatos:
-            # Convierte la línea a un número entero y agrega a la lista
-            numero = int(linea.strip())  # strip() elimina los espacios en blanco y saltos de línea
-            lista_numeros.append(numero)
-    print("\nLos números son:\n",lista_numeros,"\n")
+    #Incluimos un error en cualquier caso
+    try:
+        with open("datos.txt", "r") as archivoDatos:
+            lista_numeros = []
+            # Lee cada línea
+            for linea in archivoDatos:
+                # Convierte la línea a un número entero y agrega a la lista
+                numero = int(linea.strip())  # strip() elimina los espacios en blanco y saltos de línea
+                lista_numeros.append(numero)
+        print("\nLos números son:\n",lista_numeros,"\n")
+    except FileNotFoundError:
+        print("El archivo 'datos.txt' no existe.")  
 
     #=============================================================================
 
@@ -51,7 +74,7 @@ def main():
     else:
         print("El operador que usted ingresó no es correcto")
         print("Por favor, ingrese uno de los siguientes operadores a la próxima:")
-        print("\t - suma\n\t - resta\n\t - multiplicación\n\t - división")
+        print("\t - suma\n\t - resta\n\t - multiplicación\n\t - división\n")
         StarIf_Operación = False
 
     #II) Verificamos que el entero sea un valor entero para continuar:
@@ -67,24 +90,24 @@ def main():
     #(Si y solo si se ingresó los valores correctos)
     if ((StarIf_Operación and StarIf_Entero) == True):
         resultado = []
-
+        DivEntre0 = False #Si se divide entre 0, se convierte en True y se cancela resultado.txt
         if operacion == "suma":
             print("El resultado de la ",operacion," con ",entero," es:")
             for i in lista_numeros:
                 resultado.append(i + entero)  
-            print(resultado)   
+            print("",resultado)   
 
         elif operacion == "resta": 
             print("El resultado de la ",operacion," con ",entero," es:")
             for j in lista_numeros:
                 resultado.append(j - entero)  
-            print(resultado)   
+            print("",resultado)   
 
         elif operacion == "multiplicacion": 
             print("El resultado de la ",operacion," con ",entero," es:")
             for k in lista_numeros:
                 resultado.append(k * entero)  
-            print(resultado) 
+            print("",resultado) 
 
         elif operacion == "division":
             #Verificamos que el usuario no meta el valor 0
@@ -92,31 +115,39 @@ def main():
                 print("El resultado de la ",operacion," entre ",entero," es:")
                 for l in lista_numeros:
                     resultado.append(l / entero)  
-                print(resultado)
+                print("",resultado)
             else:
-                print("¡No se puede dividir entre cero!")
+                DivEntre0 = True
         else: 
             print("Usted no ingresó un operador válido. Error inesperado")
 
     #=============================================================================
 
         #Empezamos a guardar los resultados de la cadena en resultados.txt
-        with open("resultados.txt", "w") as archivoDatosResultados:
-            # Lee cada línea
-            for lineaR in resultado:
-                archivoDatosResultados.write(str(lineaR) + "\n")
+        #Si y solo si NO es div. entre 0
+        if (DivEntre0 == False):
+            with open("resultados.txt", "w") as archivoDatosResultados:
+                # Lee cada línea
+                for lineaR in resultado:
+                    archivoDatosResultados.write(str(lineaR) + "\n")
+            print()
+            print(lineaDeIguales)
+            print("Se han guardado los números en 'resultados.txt'.")
+        else:
+            print(lineaDeIguales)
+            print("¡No se puede dividir entre cero!")
+            print("No se pudo guardar resultados en 'resultados.txt'.")
 
-        print("\nSe han guardado los números en 'resultados.txt'.")
-
+            
     #=============================================================================
 
     #Si no inició el programa de operación por variables incorrectas
     else:
+        print(lineaDeIguales)
         print("No se pudo iniciar las operaciones debido a que se ingresaron valores incorrectos.")
         print("Por favor, inténtelo de nuevo.")
-    
     #=-=-=-=-=-
-    
+    print(lineaDeIguales)
 
 
 if __name__ == "__main__":
